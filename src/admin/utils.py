6281e4ebc4c8db234bb3119fad048b2346ee1159
AlexanderVoloshok <voloshok.a@theinvaders.pro>
2026-02-26 21:53:44 +0300

    correct token check

--- src/admin/utils.py
+++ src/admin/utils.py
@@ -9,17 +9,17 @@ def admin_only(fn):
 
     @wraps(fn)
     def wrapper(*args, **kwargs):
-        auth = request.headers.get("Authorization", "")
-        if not auth.startswith("Bearer "):
-            return jsonify({"error": "Missing or invalid Authorization header"}), 401
-        token = auth.split(" ", 1)[1].strip()
+        token = request.headers.get("Authorization", "")
+        #if not auth.startswith("Bearer "):
+        #    return jsonify({"error": "Missing or invalid Authorization header"}), 401
+        #token = auth.split(" ", 1)[1].strip()
         try:
             claims = get_jwt_identity()  # должен вернуть словарь с 'role'
         except Exception:
             return jsonify({"error": "Invalid token"}), 401
 
         role = claims.get("role")
-        if role != UserRoles.ADMIN:
+        if role != UserRoles.ADMIN.value:
             return jsonify({"error": "Admin role required"}), 403
         # Можно добавить user_id из токена в request.context при необходимости
         return fn(*args, **kwargs)
